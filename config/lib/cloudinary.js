60498049ebcd0689bad0f8012dd660afa7185695
nicknameismos <mynameissarawut@gmail.com>
2017-05-08 17:58:50 +0700

    login

---
+++ config/lib/cloudinary.js
@@ -0,0 +1,29 @@
+'use strict';
+var cloudinary = require('cloudinary');
+
+// clodinary Thamapp
+
+// cloudinary.config({
+//     cloud_name: 'hrpqiager',
+//     api_key: '576311539598718',
+//     api_secret: 'RlDrF-JO1HdapFkN2zHwtUBCerE'
+// });
+
+
+// clodinary Thamapptest
+
+// cloudinary.config({
+//     cloud_name: 'hflvlav04',
+//     api_key: '677826662678962',
+//     api_secret: 'lwKtVO7Jp6IljNJ7YEZFoFLzmMQ'
+// });
+
+// cloudinary purchasetest
+cloudinary.config({
+    cloud_name: 'hpipugeai',
+    api_key: '924436473164388',
+    api_secret: '6GG8MTxmtxqHI_W1XXwS8Ixv78k'
+});
+
+module.exports.cloudinary = cloudinary;
+
